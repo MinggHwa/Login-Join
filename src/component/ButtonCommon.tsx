51a93cce2efd8ce0c915317faf3e10f44051878d
MyoungHwaShin <shinmyounghwa92@gmail.com>
2024-07-19 23:07:27 +0900

    Feat: 회원가입 validation useForm 완료

--- src/component/ButtonCommon.tsx
+++ src/component/ButtonCommon.tsx
@@ -3,16 +3,17 @@ interface ButtonProps {
   name: string
   onClick?: (e: React.MouseEvent<HTMLButtonElement>)=> void | undefined
   onChange?: ()=> void | undefined
-  onSubmit?: (e: React.FormEvent<HTMLFormElement>)=> void | undefined
+  onSubmit?: (e?: React.BaseSyntheticEvent) => Promise<void>; 
   type?: "submit" | "reset" | "button" | undefined;
+  disabled?: boolean;
 }
 
 
-function ButtonCommon ({name, onClick,onSubmit,onChange, type}:ButtonProps) {
+function ButtonCommon ({name, onClick,onSubmit,onChange,disabled, type}:ButtonProps) {
 
 
 return (
-  <button className='bg-gray-800' onClick={onClick} onSubmit={onSubmit} onChange={onChange} type={type}>{name}</button>
+  <button className='bg-gray-800' onClick={onClick} onSubmit={onSubmit} onChange={onChange} type={type} disabled={disabled}>{name}</button>
 
 )
 
--- src/component/Join.tsx
+++ src/component/Join.tsx
@@ -1,62 +1,53 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import ButtonCommon from './ButtonCommon';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 
 interface JoinProps {
-  onClick?: ()=> void | undefined
-
-
+  onClick?: ()=> void | undefined;
+  userId: string;
+  userPw: string;
+  userPwCheck:string;
 }
 
+
 export default function Join ({}:JoinProps) {
-  const [ getValues, setGetValues ] = useState(
-    {
-      userId: '',
-      userPw: '',
-      userPwCheck: '',
-    }
-  );
+  const { register, handleSubmit,watch, formState: { errors } } = useForm<JoinProps>( {mode: 'onBlur'});
+  const regExpId = /^(?=.*[a-z])[a-zA-Z0-9_]{5,15}$/ ;
+  const regExpPw = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@!~#\$%\^&\*\(\)_\+])[A-Za-z0-9@!~#\$%\^&\*\(\)_\+]{3,30}$/;
 
+  const userPw = watch('userPw');
+  const userId = watch('userId');
+  const userPwCheck = watch('userPwCheck');
   
-  
-  const change = (e: React.ChangeEvent<HTMLInputElement>) =>{
-    let { name , value } = e.target;
+  const onSubmit: SubmitHandler<JoinProps> = data => {
+    console.log(data);
+  };
 
-    setGetValues((prev) => ({
-      ...prev,
-      [name]: value,
-  }))
-
-  }
-
-  const handleJoin = () => {
-  
-    console.log('안냐세영');
-      
-    if(getValues.userPw === getValues.userPwCheck) {
-      console.log(getValues);
-      console.log('userId:',getValues.userId);
-      console.log('userPw:',getValues.userPw);
-      console.log('userPw:',getValues.userPwCheck);
-      console.log('통과');
-    }
-      
-   
-    
-  }
 
 
   return (
   
-    <form action="#">
+    <form action="#" onSubmit={handleSubmit(onSubmit)}>
       <fieldset>
         <legend>Join NOW</legend>
         <div>
 
           <label htmlFor="Id">
-            Id:<input type="text" name="userId" value={getValues.userId} id='Id' required placeholder ='아이디' onChange={change}/>
+            Id:<input type="text" id='Id' required placeholder ='아이디' 
+            {
+              ...register('userId', { required:true, pattern: regExpId }) 
+            }
+            
+           />
+       
             
           </label>
+          {errors.userId && (<>
+            <div>아이디는 5자리 이상 15자리 이상 영문 소문자와 숫자로만 사용가능합니다.</div>
+          </>
+        
+      )}
 
         </div>
      
@@ -64,25 +55,53 @@ export default function Join ({}:JoinProps) {
        <div>
 
           <label htmlFor="Pw">
-            Pw:<input type="text" name="userPw" value={getValues.userPw} id='Pw' required placeholder ='비밀번호' onChange={change}/>
+            Pw:<input type="text" id='Pw' required placeholder ='비밀번호'
+            {
+              ...register('userPw', { required: true, pattern:regExpPw})
+
+
+            }
+            
+            
+            />
            
           </label>
+          {errors.userPw && (<>
+
+          <div>
+            비밀번호는 숫자, 영문 대문자, 영문 소문자, 특수문자 1개 이상 포함한 9자 이상 30자 이하여야 합니다.
+          </div>
+          </>)}
 
        </div>
        
        <div>
 
           <label htmlFor="PwCheck">
-            Pw:<input type="text" name="userPwCheck" value={getValues.userPwCheck}id='PwCheck' required placeholder ='비밀번호 확인' onChange={change}/>
-           
+            Pw:<input type="text" id='PwCheck' required placeholder ='비밀번호 확인'
+               {
+                ...register('userPwCheck', { required:true }) 
+    
+    
+               }
+            />
+        
           </label>
-
+           {errors.userPwCheck && (
+            <>
+            <div>비밀번호 확인은 필수입니다.</div>
+            </>)}
+            {userPw && userPwCheck && userPw !== userPwCheck && (
+              <div>
+                입력하신 비밀번호와 일치하지 않습니다.
+              </div>
+            )  }
+              
        </div>
-       <ButtonCommon name='가입하기' type='button' onClick= {handleJoin} ></ButtonCommon>
+       <ButtonCommon name='가입하기' type='submit' disabled={!userId || !userPw || userPw !== userPwCheck}></ButtonCommon>
+      
 
       </fieldset>
     </form>
 
-
-
-  )}
+)}
